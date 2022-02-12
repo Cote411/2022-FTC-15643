3147d7de5e9727db07d3de0e31e0cb6fabae441b
DhruvMadan05 <63520379+DhruvMadan05@users.noreply.github.com>
2022-02-12 08:30:31 -0500

    comp code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveCode.java
@@ -22,6 +22,10 @@ public class DriveCode extends OpMode {
         drive.turntable.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         drive.lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        drive.flipBucket.setPosition(0);
+        drive.bucketServo.setPosition(0);
+        drive.angleChanger.setPosition(1);
+        drive.flipIntake.setPosition(0);
 
 
     }
@@ -38,19 +42,28 @@ public class DriveCode extends OpMode {
         }
 
         if(gamepad2.dpad_left){
-            drive.turntable.setTargetPosition(-268);
-            drive.turntable.setPower(-0.3);
-        }else if(gamepad2.dpad_right){
             drive.turntable.setPower(0.3);
+        }else if(gamepad2.dpad_right){
+            drive.turntable.setPower(-0.3);
         }else{
             drive.turntable.setPower(0);
         }
+
+
         if(gamepad2.x){
-            drive.intake.setPower(1);
-        }else {
-            drive.intake.setPower(0);
+            drive.spinner.setPower(1);
+
+        }else{
+            drive.spinner.setPower(0);
         }
 
+        if(gamepad2.y){
+            drive.flipBucket.setPosition(0);
+            drive.bucketServo.setPosition(0);
+        }else{
+            drive.flipBucket.setPosition(1);
+            drive.bucketServo.setPosition(1);
+        }
         if(gamepad2.right_trigger != 0){
             drive.lift.setPower(1);
         }else if(gamepad2.left_trigger != 0){
@@ -58,18 +71,17 @@ public class DriveCode extends OpMode {
         }else{
             drive.lift.setPower(0);
         }
-        telemetry.addData("port for flipIntake:", drive.flipIntake.getPortNumber());
-        telemetry.update();
 
         if(gamepad2.right_bumper){
-            drive.flipIntake.setPosition(0);
-        }else{
-            drive.flipIntake.setPosition(1);
-        }
-        if(gamepad2.left_bumper){
-            drive.angleChanger.setPosition(0.25);
-        }else{
             drive.angleChanger.setPosition(1);
+        }else{
+            drive.angleChanger.setPosition(0);
+        }
+
+        if(gamepad2.left_bumper){
+            drive.flipIntake.setPosition(1);
+        }else{
+            drive.flipIntake.setPosition(0);
         }
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/RedDuckAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/RedDuckAuto.java
@@ -4,10 +4,12 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 @Autonomous(name = "Auto")
+@Disabled
 public class RedDuckAuto extends LinearOpMode {
     @Override
     public void runOpMode() {
